d7f720b1bfad9b5ac8309d2313a7c74abd0d1d9a
Joshua Levine <joshuapark95@gmail.com>
2019-03-11 03:16:00 -0700

    remove arg accents

--- src/swaps.sol
+++ src/swaps.sol
@@ -63,75 +63,75 @@ contract Swaps is DSMath, SwapEvents {
     }
 
     // --- Receiver Interface ---
-    function offer(uint256 tag_, uint256 cap_, uint256 ttl_, uint256 pot_) public {
-        uint256 due = sub(rmul(toRAY(pot_), rpow(cap_, mul(ttl_, 1 days))) / 10 ** 9, pot_);
+    function offer(uint256 tag, uint256 cap, uint256 ttl, uint256 pot) public {
+        uint256 due = sub(rmul(toRAY(pot), rpow(cap, mul(ttl, 1 days))) / 10 ** 9, pot);
 
         require(gem.transferFrom(msg.sender, address(this), due), "swaps: token transfer failed");
-        require(pot_ > 0.05 ether, "swaps: offer must have a pot of more than 0.05 gems");
+        require(pot > 0.05 ether, "swaps: offer must have a pot of more than 0.05 gems");
 
         bytes32 offerId = keccak256(
-            abi.encodePacked(msg.sender, tag_, cap_, mul(ttl_, 1 days))
+            abi.encodePacked(msg.sender, tag, cap, mul(ttl, 1 days))
         );
 
         require(pots[offerId] == 0, "swaps: pot not empty");
-        pots[offerId] = pot_;
-        emit NewOffer(msg.sender, tag_, cap_, mul(ttl_, 1 days), pot_);
+        pots[offerId] = pot;
+        emit NewOffer(msg.sender, tag, cap, mul(ttl, 1 days), pot);
     }
     // function tweak() public {}
 
     // --- Payer Interface ---
     function take(
-        address lad_, uint256 tag_, uint256 cap_, uint256 ttl_, uint256 pot_
+        address lad, uint256 tag, uint256 cap, uint256 ttl, uint256 pot
     ) public {
-        bytes32 offerId = keccak256(abi.encodePacked(lad_, tag_, cap_, ttl_));
+        bytes32 offerId = keccak256(abi.encodePacked(lad, tag, cap, ttl));
         bytes32 swapId = keccak256(
-            abi.encodePacked(lad_, msg.sender, tag_, cap_, ttl_, now, pot_, tub.rhi())
+            abi.encodePacked(lad, msg.sender, tag, cap, ttl, now, pot, tub.rhi())
         );
 
         require(pots[offerId] > 0, "swaps: empty pot");
-        require(pot_ > 0.05 ether,  "swaps: swap must have a pot of more than 0.05 gems");
-        pots[offerId] = sub(pots[offerId], pot_);
-        uint256 due = sub(rmul(toRAY(pot_), rpow(tag_, ttl_)) / 10 ** 9, pot_);
+        require(pot > 0.05 ether,  "swaps: swap must have a pot of more than 0.05 gems");
+        pots[offerId] = sub(pots[offerId], pot);
+        uint256 due = sub(rmul(toRAY(pot), rpow(tag, ttl)) / 10 ** 9, pot);
         require(gem.transferFrom(address(msg.sender), address(this), due), "swaps: token transfer failed");
 
         swaps[swapId] = true;
-        emit NewSwap(lad_, msg.sender, tag_, cap_, ttl_, now, pot_, tub.rhi());
+        emit NewSwap(lad, msg.sender, tag, cap, ttl, now, pot, tub.rhi());
     }
 
     // --- Settlement ---
     function settle(
-        address lad_, address gal_, uint256 pot_, uint256 tag_, 
-        uint256 cap_, uint256 rhi_, uint256 start_, uint256 ttl_
+        address lad, address gal, uint256 pot, uint256 tag, 
+        uint256 cap, uint256 rhi, uint256 start, uint256 ttl
     ) public {
         bytes32 swapId = keccak256(
-            abi.encodePacked(lad_, gal_, tag_, cap_, ttl_, start_, pot_, rhi_)
+            abi.encodePacked(lad, gal, tag, cap, ttl, start, pot, rhi)
         );
         
         require(swaps[swapId], "swaps: swap id must be of an active swap");
-        require(now >= add(start_, ttl_), "swaps: swap must be past maturity");
+        require(now >= add(start, ttl), "swaps: swap must be past maturity");
 
-        uint256 accruedInterest = wmul(pot_, sub(tub.rhi(), rhi_)) / 10 ** 9;
-        uint256 takerPool = sub(rmul(toRAY(pot_), rpow(tag_, ttl_)) / 10 ** 9, pot_);
-        uint256 providerPool = sub(rmul(toRAY(pot_), rpow(cap_, ttl_)) / 10 ** 9, pot_);
+        uint256 accruedInterest = wmul(pot, sub(tub.rhi(), rhi)) / 10 ** 9;
+        uint256 takerPool = sub(rmul(toRAY(pot), rpow(tag, ttl)) / 10 ** 9, pot);
+        uint256 providerPool = sub(rmul(toRAY(pot), rpow(cap, ttl)) / 10 ** 9, pot);
         
         // if the accrued interest is more than what the taker pooled, the rate moved against the provider
         if (takerPool < accruedInterest) {
             // if the provider owes more than what they pooled, the taker gets whats there
             if (sub(accruedInterest, takerPool) > providerPool) {
-                require(gem.transfer(gal_, add(takerPool, providerPool)), "swaps: token transfer failed");
+                require(gem.transfer(gal, add(takerPool, providerPool)), "swaps: token transfer failed");
             } else {
                 // otherwise both parties get a payout, but the taker comes out ahead
-                require(gem.transfer(gal_, accruedInterest), "swaps: token transfer failed");
-                require(gem.transfer(lad_, sub(providerPool, sub(accruedInterest, takerPool))), "swaps: token transfer failed");
+                require(gem.transfer(gal, accruedInterest), "swaps: token transfer failed");
+                require(gem.transfer(lad, sub(providerPool, sub(accruedInterest, takerPool))), "swaps: token transfer failed");
             }
         } else {
             // if the accrued interest is less than or equal to what the taker pooled, the provider comes out ahead
-            require(gem.transfer(gal_, accruedInterest), "swaps: token transfer failed");
-            require(gem.transfer(lad_, add(sub(takerPool, accruedInterest), providerPool)), "swaps: token transfer failed");
+            require(gem.transfer(gal, accruedInterest), "swaps: token transfer failed");
+            require(gem.transfer(lad, add(sub(takerPool, accruedInterest), providerPool)), "swaps: token transfer failed");
         }
 
         swaps[swapId] = false;
-        emit Settled(lad_, gal_);
+        emit Settled(lad, gal);
     }
 
     function toRAY(uint256 wad) internal pure returns(uint256 _ray) {
